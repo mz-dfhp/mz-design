85d9cbfdc66a4192f600abfc3032cb47b0a9c1b4
lc <486836332@qq.com>
2023-06-14 16:33:19 +0800

    feat: build github

--- docs/.dumirc.ts
+++ docs/.dumirc.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'dumi'
-
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
+    base: process.env.base || '/',
+    publicPath: process.env.base || '/',
     name: 'design',
     socialLinks: { github: 'https://github.com/mz-dfhp/mz-design' },
-    monorepoRedirect: {}
+    monorepoRedirect: {},
+    footer: false
   }
 })
