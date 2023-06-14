f44bbdf40f0f2fb9a815b89ac869bf7b29fa5976
lc <486836332@qq.com>
2023-06-14 16:49:13 +0800

    fix: config bug

--- docs/.dumirc.ts
+++ docs/.dumirc.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'dumi'
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: process.env.base || '/',
+  publicPath: process.env.base || '/',
   themeConfig: {
-    base: process.env.base || '/',
-    publicPath: process.env.base || '/',
     name: 'design',
     socialLinks: { github: 'https://github.com/mz-dfhp/mz-design' },
     monorepoRedirect: {},
